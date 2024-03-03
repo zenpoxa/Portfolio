e22c575b1ba440feda8698a98a6801870c460e78
Zenpoxa <zenpoxa@gmail.com>
2024-03-03 22:42:56 +0100

    ajout envoi de mail (prémices du back)

--- Functions/contact.js
+++ Functions/contact.js
@@ -1,49 +1,31 @@
-// $errors = [];
+// ENVOI DU MAIL
+(function() {
+    emailjs.init('dei_TgZWrVbhtIIsj');
+})();
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     // Get POST data
-//     $name = isset($_POST['name']) ? strip_tags(trim($_POST['name'])) : '';
-//     $email = isset($_POST['email']) ? trim($_POST['email']) : '';
-//     $message = isset($_POST['message']) ? strip_tags(trim($_POST['message'])) : '';
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-//     // Validate form fields
-//     if (empty($name)) {
-//         $errors[] = 'Name is empty';
-//     }
+        // vérifier si les champs sont bien remplis
+        if (
+            !this.querySelector("#name").value ||
+            !this.querySelector("#email").value ||
+            !this.querySelector("textarea").value        
+        )
+        {
+            console.log("Champs mal remplis");
+        }
 
-//     if (empty($email)) {
-//         $errors[] = 'Email is empty';
-//     } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-//         $errors[] = 'Email is invalid';
-//     }
+        else {
+            emailjs.sendForm('service_a49mbi8', 'template_vd90ozf', this)
+            .then(function() {
+                console.log('Mail envoyé avec succès!');
+            }, function(error) {
+                console.log('ERREUR : Mail pas envoyé...', error);
+            });
+        }
 
-//     if (empty($message)) {
-//         $errors[] = 'Message is empty';
-//     }
-
-//     // If no errors, send email
-//     if (empty($errors)) {
-//         // Recipient email address (replace with your own)
-//         $recipient = "zenpoxa@gmail.com";
-
-//         // Additional headers
-//         $headers = "From: $name <$email>";
-
-//         // Send email
-//         if (mail($recipient, $message, $headers)) {
-//             echo "Email sent successfully!";
-//         } else {
-//             echo "Failed to send email. Please try again later.";
-//         }
-//     } else {
-//         // Display errors
-//         echo "The form contains the following errors:<br>";
-//         foreach ($errors as $error) {
-//             echo "- $error<br>";
-//         }
-//     }
-// } else {
-//     // Not a POST request, display a 403 forbidden error
-//     header("HTTP/1.1 403 Forbidden");
-//     echo "You are not allowed to access this page.";
-// } -->
+        
+    });
+}
