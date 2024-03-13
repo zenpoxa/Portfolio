39287b0c108d4c0adc949d7c4b3a9cc9d546b06a
Zenpoxa <zenpoxa@gmail.com>
2024-03-13 23:00:05 +0100

    contact amélioré (feedback envoi du msg)

--- Functions/contact.js
+++ Functions/contact.js
@@ -3,26 +3,33 @@
     emailjs.init('dei_TgZWrVbhtIIsj');
 })();
 
+var resultText = document.getElementById("result");
+
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
+        resultText.innerHTML = "";
+        resultText.className = "";
 
         // vérifier si les champs sont bien remplis
         if (
             !this.querySelector("#name").value ||
             !this.querySelector("#email").value ||
-            !this.querySelector("textarea").value        
+            !this.querySelector("textarea").value
         )
         {
-            console.log("Champs mal remplis");
+            resultText.classList.add("error");
+            resultText.innerHTML = "Champs mal remplis";
         }
 
         else {
             emailjs.sendForm('service_a49mbi8', 'template_vd90ozf', this)
             .then(function() {
-                console.log('Mail envoyé avec succès!');
+                resultText.classList.add("success")
+                resultText.innerHTML = "Envoyé !";
             }, function(error) {
-                console.log('ERREUR : Mail pas envoyé...', error);
+                resultText.classList.add("error")
+                resultText.innerHTML = "Erreur serveur...";
             });
         }
     });
