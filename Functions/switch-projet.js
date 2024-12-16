276b1b62e39a87e06a61b708b5e904ea25ec15e9
richard <zenpoxa@gmail.com>
2024-12-16 11:22:32 +0100

    refactor: enlever certains projets

--- Functions/switch-projet.js
+++ Functions/switch-projet.js
@@ -1,23 +1,23 @@
-const tableau_pages = ["projet-tabas-kho.html", "projet-portfolio.html", "projet-documentation.html", "projet-resolveur.html", "projet-interview.html", "projet-gestion.html", "projet-bdd.html"];
-var currentPage = window.location.href.split('/'); currentPage = (currentPage[currentPage.length-1]);
+const tableau_pages = ["projet-tabas-kho.html", "projet-portfolio.html", "projet-interview.html", "projet-bdd.html"];
+var currentPage = window.location.href.split('/'); currentPage = (currentPage[currentPage.length - 1]);
 var currentIdx = tableau_pages.indexOf(currentPage);
 
 function nextProject() {
-    if (currentIdx == tableau_pages.length-1) {
+    if (currentIdx == tableau_pages.length - 1) {
         window.location.href = "/Pages/" + tableau_pages[0];
     }
     else {
-        window.location.href = "/Pages/" + tableau_pages[currentIdx+1];
+        window.location.href = "/Pages/" + tableau_pages[currentIdx + 1];
     }
 }
 
 function prevProject() {
     if (currentIdx == 0) {
-        window.location.href = "/Pages/" + tableau_pages[tableau_pages.length-1];
+        window.location.href = "/Pages/" + tableau_pages[tableau_pages.length - 1];
     }
     else {
-        window.location.href = "/Pages/" + tableau_pages[currentIdx-1];
+        window.location.href = "/Pages/" + tableau_pages[currentIdx - 1];
     }
 }
 
-export {prevProject, nextProject};
+export { prevProject, nextProject };
